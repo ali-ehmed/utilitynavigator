33ec4eebb29cf3586eb047649f6622fce27db1d7
Ali Ahmed <ali_ahmed922@outlook.com>
2016-02-15 22:47:32 +0500

    changes for mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "designhenge.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "ali.ahmed.cs2016@gmail.com",
-    password: "Gumball2016"
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "designhenge.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: "ali.ahmed.cs2016@gmail.com",
+  #   password: "Gumball2016"
+  # }
 end
