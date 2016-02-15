795bdf2f7b1afa580a73651fd5f5f56f9a0f6b3d
Ali Ahmed <ali_ahmed922@outlook.com>
2016-02-15 22:52:13 +0500

    changes for mandrill

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def make_action_mailer_use_request_host_and_protocol
     ActionMailer::Base.default_url_options[:protocol] = request.protocol
-    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+    ActionMailer::Base.default_url_options[:host] = request.host
   end
 end
