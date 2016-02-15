f8593e78e07d617c236813b5e8cd2c7589477916
Ali Ahmed <ali_ahmed922@outlook.com>
2016-02-15 23:43:31 +0500

    figaro gem for environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: "designhenge.com",
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: "ali.ahmed.cs2016@gmail.com",
-  #   password: "Gumball2016"
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: UtilityNavigator::Application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: UtilityNavigator::Application.secrets.user_name,
+    password: UtilityNavigator::Application.secrets.password
+  }
 end
