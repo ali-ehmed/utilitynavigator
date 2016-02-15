7a7e8eaa92aa2f8200078761c905c4ea37c0d73f
Ali Ahmed <ali_ahmed922@outlook.com>
2016-02-15 22:21:15 +0500

    mandrill configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => UtilityNavigator::Application.secrets.mandrill_username,
+    :password  => UtilityNavigator::Application.secrets.mandrill_key
+  }
 end
