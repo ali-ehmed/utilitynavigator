ba64daf84de3fff5d0ddf2a9a30d1a978d7824f8
Ali Ahmed <ali_ahmed922@outlook.com>
2016-02-17 14:42:11 +0500

    active admin cms setup

---
+++ app/admin/user.rb
@@ -0,0 +1,37 @@
+ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation
+  menu priority: 1
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    column :confirm_user do |user|
+      link_to "Confirm", confirm_user_admin_user_path(user), method: :put
+    end
+    actions
+  end
+
+  filter :email
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
+  member_action :confirm_user, method: :put do
+    resource.confirm!
+    redirect_to admin_users_path, notice: "#{resource.email} has been confirmed!"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Admin::Service.new.call
+puts 'CREATED SUPER ADMIN USER: ' << admin.email
