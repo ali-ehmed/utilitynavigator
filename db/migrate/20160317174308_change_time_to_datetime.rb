4b09513092b4fa9a4ce682912f70ea91cafc8423
aliahmed922 <ali_ahmed922@outlook.com>
2016-03-18 00:06:33 +0500

    migration changes

--- db/migrate/20160317174308_change_time_to_datetime.rb
+++ db/migrate/20160317174308_change_time_to_datetime.rb
@@ -1,7 +1,7 @@
 class ChangeTimeToDatetime < ActiveRecord::Migration
   def up
   	# remove_column :call_backs, :preferred_time
-    add_column :call_backs, :preferred_time, :time
+    # add_column :call_backs, :preferred_time, :time
   end
 
   def down
