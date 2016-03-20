0aa34d6133e2009a5ee53419e4f21e2183ddd756
aliahmed922 <ali_ahmed922@outlook.com>
2016-03-21 01:08:08 +0500

    some changes fixed

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -23,7 +23,7 @@ class Payment < ActiveRecord::Base
 	belongs_to :package
 
 	validates :card_last4, :card_exp_year, :card_exp_month, :security_code, presence: true, if: :payment_after_install
-	validates :card_last4, format: { :with => /[\d-]/, message: "must be like 99999-9999-9999-9" }
+	validates :card_last4, format: { :with => /[\d-]/, message: "must be like 99999-9999-9999-9" }, if: :payment_after_install
 	
 	after_create :send_admin_notification, :send_user_notification
 
