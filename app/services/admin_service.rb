dec79d7ee3139cc4f009238a81d1a91056e62b38
Ali Ahmed <ali_ahmed922@outlook.com>
2016-03-31 18:32:14 +0500

    admin users added

--- app/services/admin_service.rb
+++ app/services/admin_service.rb
@@ -1,9 +1,16 @@
 class AdminService
   def call
-    admin = AdminUser.find_or_create_by!(email: "admin@example.com") do |admin|
-      admin.password = "admin123"
-      admin.password_confirmation = "admin123"
-      admin.super_admin = true
+    admin = AdminUser.find_or_create_by!(email: "khurram.chaudhry@excelsteer.com") do |admin|
+      admin.password = "BeHappy2016#"
+      admin.password_confirmation = "BeHappy2016"
+      admin.super_admin = false
+    end
+
+    # Developers
+    admin = AdminUser.find_or_create_by!(email: "developers@designhenge.com") do |admin|
+      admin.password = "testing123"
+      admin.password_confirmation = "testing123"
+      admin.super_admin = false
     end
   end
 end
